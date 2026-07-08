5a33a747f11af443061197f00df72a9140de5f84
tayyab <tayyab.shahjahan@gmail.com>
2026-07-09 02:34:24 +0500

    new changes in compose

--- Backend/app.js
+++ Backend/app.js
@@ -22,7 +22,7 @@ mongoose
 
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "..", "frontEnd", "views"));
+app.set("views", path.join(__dirname, "..", "FrontEnd", "views"));
 
 app.use(express.static(path.join(__dirname, "../frontend/public")));
 
